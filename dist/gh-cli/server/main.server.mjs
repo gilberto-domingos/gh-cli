033699531c322d7f1c6f68bac1a3cb259d97d987
gilberto-domingos <domingoshot@hotmail.com>
2024-06-03 19:46:48 -0300

    chage code style home

--- dist/gh-cli/server/main.server.mjs
+++ dist/gh-cli/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4JI4EUHK.mjs";import"./chunk-BH5TQVSC.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-BAY64PPC.mjs";import"./chunk-BH5TQVSC.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
