df027cfed6c01b100ff0ff3280e8e3d77db33f5e
gilberto-domingos <domingoshot@hotmail.com>
2025-06-08 15:01:02 -0300

    chore: public

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
-import { CarouselComponent} from "../carousel/carousel.component";
-import { CokieComponent} from "../cokie/cokie.component";
+import { CommonModule } from '@angular/common'; // <--- IMPORTANTE
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { CarouselComponent } from '../carousel/carousel.component';
+import { CokieComponent } from '../cokie/cokie.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, CokieComponent],
+  imports: [
+    CommonModule,
+    CarouselComponent,
+    CokieComponent,
+    MatButtonToggleModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  expanded = false;
+  maxHeight = 0;
 
+  @ViewChild('stripeImage', { static: false })
+  stripeImage!: ElementRef<HTMLImageElement>;
+  @ViewChild('textContent', { static: false }) textContent!: ElementRef;
+
+  ngAfterViewInit(): void {
+    const image = this.stripeImage.nativeElement;
+
+    if (image.complete) {
+      this.setMaxHeight();
+    } else {
+      image.onload = () => {
+        this.setMaxHeight();
+      };
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    if (!this.expanded) {
+      this.setMaxHeight();
+    }
+  }
+
+  toggleExpand(): void {
+    this.expanded = !this.expanded;
+    if (!this.expanded) {
+      this.setMaxHeight();
+    }
+  }
+
+  private setMaxHeight(): void {
+    if (this.stripeImage && this.stripeImage.nativeElement) {
+      const height = this.stripeImage.nativeElement.clientHeight;
+      this.maxHeight = height - 20;
+    }
+  }
 }
---
+++ src/app/services/dialog-service/dialog-service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-service',
+  imports: [],
+  templateUrl: './dialog-service.component.html',
+  styleUrl: './dialog-service.component.scss'
+})
+export class DialogServiceComponent {
+
+}
